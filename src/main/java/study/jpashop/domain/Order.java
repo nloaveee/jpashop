bc8a1b151cf73993923e1cf13dbc8b3e3362db2e
hyeonheee <nloavee@gmail.com>
2024-10-10 19:00:44 +0900

    entity

--- src/main/java/study/jpashop/domain/Order.java
+++ src/main/java/study/jpashop/domain/Order.java
@@ -26,7 +26,7 @@ public class Order {
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne (fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name="delivery_id")
     private Delivery delivery;
 
@@ -34,6 +34,23 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus staus;  // 주문 상태  [ORDER, CANCEL]
+
+    //==연관관계 메서드==//
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 
 
